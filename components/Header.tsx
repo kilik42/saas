275f13441dc5355f6c4df359d9e293a0ff383797
kilik42 <marvinevins@yahoo.com>
2023-11-06 05:28:09 -0600

    first commit

---
+++ components/Header.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Logo from './Logo'
+
+function Header() {
+  return (
+    <header>
+      <nav>
+        {/* logo */}
+        <Logo />
+      </nav>
+    </header>
+  )
+}
+
+export default Header
